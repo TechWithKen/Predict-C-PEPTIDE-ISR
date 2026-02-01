e583d073d72708a369785b6caa1c0bf06d0f5aad
TechWithKen <ti664469@gmail.com>
2026-02-01 21:42:13 +0100

    extracted the selected features to increase model metrics

--- pep-isr.py
+++ pep-isr.py
@@ -1,16 +1,14 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor, StackingRegressor
 from sklearn.linear_model import Lasso
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from xgboost import XGBRegressor
+from sklearn.compose import ColumnTransformer
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error, root_mean_squared_error, mean_absolute_percentage_error
@@ -22,57 +20,77 @@ dataset_new = pd.get_dummies(dataset, columns=['Family_History_of_Diabetes', 'Ge
 'Physical_Activity', 'Smoking', 'Alcohol_Consumption',
        'Obesity', 'Diet', 'Sleep_Apnea', 'Diabetes_Status', 'Hypertension', 'PCOS'])
 
-
-X = dataset_new
-y = dataset_label
-
-scaler = StandardScaler()
-X_another = scaler.fit_transform(X)
-print(X_another)
-
-model = Lasso(alpha=0.01)
-model.fit(X_another, y)
-selected_features = X.columns[model.coef_ != 0]
-X_new = X[selected_features]
-
-
-X_train, X_test, y_train, y_test = train_test_split(X_new, y, test_size=0.2, random_state=42)
-
-
-base_models = [
-    ("rdf", RandomForestRegressor(n_estimators=100, max_features="sqrt", min_samples_leaf=2, max_depth=5, random_state=42)),
-    ("kn", KNeighborsRegressor(n_neighbors=5, weights="distance")),
-    ("xgb", XGBRegressor(n_estimators=300, learning_rate=0.1, subsample=1.0, max_depth=5, random_state=42))
+selected_features = [
+    "Diabetes_Status_Negative",
+    "Family_History_of_Diabetes_No",
+    "LDL_Cholesterol",
+    "Proinsulin_Levels",
+    "OGTT",
+    "Microalbuminuria",
+    "AST",
+    "Random_Blood_Glucose",
+    "CRP_Levels",
+    "Fasting_Blood_Glucose",
+    "Triglyceride_Levels",
+    "Blood_Pressure_Diastolic",
+    "Creatinine_Levels",
+    "Fructosamine_Levels",
+    "Blood_Pressure_Systolic",
+    "Hypertension_No",
+    "Postprandial_Blood_Glucose",
+    "ALT",
+    "eGFR",
+    "Uric_Acid_Levels",
+    "Gestational_Diabetes_No",
+    "Physical_Activity_No",
+    "Obesity_No",
+    "PCOS_No",
+    "BMI",
+    "Diet_No",
+    "Waist_Circumference",
+    "HOMA_IR",
+    "HbA1c",
+    "Smoking_No"
 ]
 
 
-meta_model = LinearRegression()
+def feature_engineering(dataset, label):
+    X = dataset
+    y = label
 
-stacked_model = StackingRegressor(estimators=base_models,
-                                   final_estimator=meta_model, passthrough=True)
-
-xgbmodel = Pipeline(steps=[
-    ("model", stacked_model)
-])
-
-xgbmodel.fit(X_train, y_train)
-prediction = xgbmodel.predict(X_test)
-
-print(root_mean_squared_error(y_test, prediction))
-print(mean_absolute_percentage_error(y_test, prediction))
+    X_new = X[selected_features]
+    
+    return X_new, y
 
 
-target = y_test  # replace 'target' with your column name
+def train_model(new_dataset, label):
+    preprocessing = ColumnTransformer(transformers=[
+        ("scale", StandardScaler(), selected_features)
+    ])
+    X_train, X_test, y_train, y_test = train_test_split(new_dataset, label, test_size=0.2, random_state=42)
+    print(X_train)
+    base_models = [
+        ("rdf", RandomForestRegressor(n_estimators=200, max_features="sqrt", min_samples_leaf=3, max_depth=5, random_state=42)),
+        ("svr", SVR(kernel='rbf', C=1, gamma="scale")),
+        ("lin", LinearRegression()),
+        ("knn", KNeighborsRegressor(n_neighbors=5, weights="uniform"))
+    ]
 
-min_val = target.min()
-max_val = target.max()
-range_val = max_val - min_val
+    meta_model = RandomForestRegressor(n_estimators=200, max_features="sqrt", min_samples_leaf=3, max_depth=5, random_state=42)
+    stacked_model = StackingRegressor(estimators=base_models,
+                                    final_estimator=meta_model, passthrough=True)
 
-print("Minimum:", min_val)
-print("Maximum:", max_val)
-print("Range:", range_val)
+    regression_model = Pipeline(steps=[
+        ("preprocess", preprocessing),
+        ("model", stacked_model)
+    ])
+    regression_model.fit(X_train, y_train)
+    prediction = regression_model.predict(X_test)
 
-# Optional: calculate RMSE as percentage of range
-rmse = root_mean_squared_error(y_test, prediction)
-rmse_percent = (rmse / range_val) * 100
-print(f"RMSE is {rmse_percent:.2f}% of the target range")
+    return y_test, prediction
+
+data = feature_engineering(dataset_new, dataset_label)
+model_prediction = train_model(data[0], data[1])
+
+print(f'RMSE - {root_mean_squared_error(model_prediction[0], model_prediction[1])}')
+print(f'MSE - {mean_squared_error(model_prediction[0], model_prediction[1])}')
